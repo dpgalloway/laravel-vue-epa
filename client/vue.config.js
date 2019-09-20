72ee05d56904ac796030a2b15d259633258c49c7
Devin Galloway <Devin.Galloway@erg.com>
2019-09-20 16:50:49 -0600

    Update config to get WSDS scss working within the app

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+
 module.exports = {
   runtimeCompiler: true,
   lintOnSave: false,
   publicPath: process.env.NODE_ENV === 'production' ? 'client/dist/' : '/',
+  css: {
+    loaderOptions: {
+      sass: {
+        includePaths: [
+          path.resolve(__dirname, 'node_modules/uswds/dist/scss'),
+          path.resolve(__dirname, 'node_modules/uswds/dist/scss/packages'),
+          path.resolve(__dirname, 'node_modules/uswds/dist/scss/theme'),
+        ],
+      },
+    },
+  },
 };
